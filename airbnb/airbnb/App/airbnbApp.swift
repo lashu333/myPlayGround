e0a70013859cfa33bb3571759eceb17c80fb43bc
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-10-23 23:03:06 +0400

    Initial Commit

---
+++ airbnb/airbnb/App/airbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbApp.swift
+//  airbnb
+//
+//  Created by Lasha Tavberidze on 28.09.24.
+//
+
+import SwiftUI
+
+@main
+struct airbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
